e5b2737441cc293188f1de7a19a411ddac4311b1
Animesh Nath <69345507+n-ce@users.noreply.github.com>
2022-01-26 21:05:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const click = 0;
 
 function call() {
   click++;
-  if (click > 1){sound.stop();}
+  if (click > 2){sound.stop();}
   navigator.clipboard.readText().then(Cliplink => { link = Cliplink });
   var sound = new Howl({
     src: ["https://projectlounge.pw/ytdl/download?url=" + link + "&format=249"],
