dc4d3852f0c8ab1ab01ecc25a8f9992cf006267d
n-ce <abrocks1120@gmail.com>
2022-01-27 12:29:26 +0530

    v1.8 : Added Player Controls & Removed Howler

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const loader = document.querySelector('div');
-let click = 0;
-navigator.clipboard.readText().then(Cliplink => { link = Cliplink });
+
+
 document.querySelector('p').onclick =
   function() {
-    click++;
-    var sound = new Howl({
-      src: ["https://projectlounge.pw/ytdl/download?url=" + link + "&format=249"],
-      html5: true,
-      onend: function() {
-        location.reload();
-      }
+    //copies from clipboard
+    navigator.clipboard.readText().then(link => {
+
+      document.querySelector('audio').src = "https://projectlounge.pw/ytdl/download?url=" + link + "&format=249";
+
+      //Thumbnail
+      let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
+      let id = link.match(re)[7];
+      document.querySelector('img').src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
+
     });
-    if (click % 2 == 1) {
-      sound.play();
-    }
-    else {
-      location.reload();
-    }
+    // Make the Loader Visible
     loader.style.borderTopColor = "hotpink";
-    setTimeout(animation, 20000);
-    let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
-    let id = link.match(re)[7];
-    document.querySelector('img').src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
+
+    //Loading Animation Duration
+    setTimeout(animation, 10000);
   }
 
 function animation() {
