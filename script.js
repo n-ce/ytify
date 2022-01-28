f9b38a04b432b2bc4c283d6525bbb9d2a7446b7c
n-ce <abrocks1120@gmail.com>
2022-01-28 09:49:52 +0530

    Maintenance, UI, Codebase Updates

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const audio = document.querySelector('audio');
+const thumb = document.querySelector('img');
+const a1 = "https://projectlounge.pw/ytdl/download?url=https://youtu.be/";
+const a2 = "&format=249";
+const t1 = "https://img.youtube.com/vi/";
+const t2 = "/maxresdefault.jpg";
 
 function script() {
   //copies from clipboard
   navigator.clipboard.readText().then(link => {
-
-    audio.src = "https://projectlounge.pw/ytdl/download?url=" + link + "&format=249";
+    //UID Extractor
+    let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
+    let id = link.match(re)[7];
+    //Playback
+    audio.src = a1 + id + a2;
     audio.play();
     //Thumbnail
-    let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
-    id = link.match(re)[7];
-    document.querySelector('img').src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
-
+    thumb.src = t1 + id + t2;
   });
 }
 
 script();
 
-document.querySelector('img').onclick = (e) => {
-  script();
-}
+thumb.onclick = (e) => { script() }
