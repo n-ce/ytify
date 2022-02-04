aecb975ee44580adf81cfaca5f759dd3beadb74f
n-ce <abrocks1120@gmail.com>
2022-02-04 14:35:02 +0530

    v2f : interface changes, code complexity decreased

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-if (localStorage.getItem("2e") == null) {
-  alert('YTIFY 2e\n\nCopy YT Video link to Clipboard to start playing automatically.\n\nHow To queue :\nEnable Enqueue, copy next track link and click on plus button.\n\nComing Soon :\nFirefox Browser Support.');
+if (localStorage.getItem("2f") == null) {
+  alert('YTIFY 2f\n\nCopy YT Video link to Clipboard to start playing automatically.\n\nHow To Queue :\nClick Queue, copy next track link and click on plus button.');
   localStorage.clear();
-  localStorage.setItem("2e", "yes");
+  localStorage.setItem("2f", "yes");
 }
 
 const button = document.getElementsByClassName('btn');
@@ -33,7 +33,7 @@ function atsrc(x) {
 
 // save hq setting
 if (localStorage.getItem('format') == "yes") {
-  input[1].checked = true;
+  input[3].checked = true;
   c = 251;
   q = "high";
 }
@@ -45,13 +45,13 @@ function script() {
     let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
     let id = link.match(re)[7];
     // HQ 128kbps
-    if (input[1].checked == true && q == "low") {
+    if (input[3].checked == true && q == "low") {
       c = 251;
       atsrc(id);
       q = "high";
       localStorage.setItem('format', "yes");
     }
-    else if (input[1].checked == false && q == "high") {
+    else if (input[3].checked == false && q == "high") {
       c = 249;
       atsrc(id);
       q = "low";
@@ -84,54 +84,68 @@ button[1].addEventListener("click", function() {
   audio.currentTime += 10;
 })
 // next track
-button[4].addEventListener("click", function() {
+button[3].addEventListener("click", function() {
   atsrc(array[n]);
   n++;
 });
-
-// enable queue
-
+// store new id in queue
 button[2].addEventListener("click", function() {
-  button[2].classList.add('bg-primary');
-  if (m == null) {
-    button[3].classList.remove('disabled');
-    button[4].classList.remove('disabled');
-    input[0].disabled = queue = true;
-    m = 0;
-    clearInterval(interval);
-    script();
-  }
-  else { location.reload(); }
+  script();
 });
 
-button[3].addEventListener("click", function() {
+// Queue Loop Auto Save
+
+if (localStorage.getItem('play') == "loop") {
+  audio.onended = (e) => {
+    audio.play();
+  };
+  input[1].checked = true;
+}
+else if (localStorage.getItem('play') == "queue") {
+  m = 0;
+  queue = true;
+  clearInterval(interval);
   script();
+  input[0].checked = true;
+  button[2].classList.remove('d-none');
+  button[3].classList.remove('d-none');
+  label.classList.remove('d-none');
+}
+else {
+  audio.onended = null;
+  localStorage.setItem('play', "auto");
+  input[2].checked = true;
+}
+
+// queue
+
+input[0].addEventListener("click", function() {
+  m = 0;
+  queue = true;
+  clearInterval(interval);
+  script();
+  button[2].classList.remove('d-none');
+  button[3].classList.remove('d-none');
+  label.classList.remove('d-none');
+  localStorage.setItem('play', "queue");
 });
 
 //Loop
 
-if (localStorage.getItem('loop') == "yes") {
-  input[0].checked = true;
+input[1].addEventListener("click", function() {
   audio.onended = (e) => {
     audio.play();
   };
-  button[2].disabled = true;
-}
-
-input[0].addEventListener("click", function() {
-  if (input[0].checked == true) {
-    audio.onended = (e) => {
-      audio.play();
-    };
-    button[2].disabled = true;
-    localStorage.setItem('loop', "yes");
-  }
-  else {
-    audio.onended = null;
-    button[2].disabled = false;
-    localStorage.setItem('loop', "no");
-  }
+  localStorage.setItem('play', "loop");
+  location.reload();
 });
+
+// auto
+input[2].addEventListener("click", function() {
+  localStorage.setItem('play', "auto");
+  location.reload();
+});
+
 
 // Dark Mode
 
@@ -139,8 +153,8 @@ if (theme == "dark") {
   body.remove('bg-secondary');
   body.add('bg-dark');
 }
-input[2].checked = theme == "dark" ? true : false;
-input[2].onchange = function() {
+input[4].checked = theme == "dark" ? true : false;
+input[4].onchange = function() {
   if (this.checked) {
     body.remove('bg-secondary');
     body.add('bg-dark');
@@ -153,7 +167,7 @@ input[2].onchange = function() {
 }
 
 // clear settings
-button[5].addEventListener("click", function()
+button[4].addEventListener("click", function()
 {
   localStorage.clear();
   location.reload();
