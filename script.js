4979ab49fdfbd4d612fe2e7cbd7992ebce174737
n-ce <abrocks1120@gmail.com>
2022-02-05 21:55:41 +0530

    fix #21

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ const body = document.body.classList;
 const array = []; // id storage
 const interval = setInterval(script, 2000);
 
+const abstract = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
+
 let y; // store id
 let m; // queue count 
+let link;
+let param;
 let n = 1; // current queue playing
 let c = 249; // quality value
-let q = "low"; // quality boolean
 let queue = false; // queue boolean
 let error = "NotAllowedError: Read permission denied.";
 
@@ -20,9 +23,14 @@ let error = "NotAllowedError: Read permission denied.";
 if (navigator.userAgent.indexOf('Firefox') != -1) {
   input[0].classList.remove('d-none');
   alert('Clipboard API Not Supported : Some Functions Might Not Work');
+  error = true;
   clearInterval(interval);
 }
 
+// UID EXTRACTOR
+function caller(val) {
+  return val.match(abstract)[7];
+}
 
 function atsrc(x) {
   //Playback
@@ -37,44 +45,45 @@ function atsrc(x) {
 if (localStorage.getItem('format') == "yes") {
   input[4].checked = true;
   c = 251;
-  q = "high";
+}
+
+// HQ 128kbps
+
+input[4].addEventListener("click", function() {
+  if (input[4].checked == true) {
+    c = 251;
+    atsrc(param);
+    localStorage.setItem('format', "yes");
+  }
+  else if (input[4].checked == false) {
+    c = 249;
+    atsrc(param);
+    localStorage.setItem('format', "no");
+  }
+});
+
+function algorithm(param) {
+  //initial id value
+  if (y == undefined) { atsrc(param); }
+  //start playing if new id
+  else if (y != param && queue == false) {
+  }
+  // queue new id
+  else if (y != param && queue == true) {
+    m++;
+    label.innerText = m - n + 1;
+    array[m] = y = param;
+    audio.onended = (e) => {
+      atsrc(array[n]);
+      label.innerText = m - n;
+      n++;
+    }
+  }
 }
 
 
 function script() {
-  navigator.clipboard.readText().then(link => {
-    //UID Extractor
-    let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
-    let id = link.match(re)[7];
-    // HQ 128kbps
-    if (input[4].checked == true && q == "low") {
-      c = 251;
-      atsrc(id);
-      q = "high";
-      localStorage.setItem('format', "yes");
-    }
-    else if (input[4].checked == false && q == "high") {
-      c = 249;
-      atsrc(id);
-      q = "low";
-      localStorage.setItem('format', "no");
-    }
-    //initial id value
-    if (y == undefined) { atsrc(id); }
-    //start playing if new id
-    else if (y != id && queue == false) { atsrc(id); }
-    // queue new id
-    else if (y != id && queue == true) {
-      m++;
-      label.innerText = m - n + 1;
-      array[m] = y = id;
-      audio.onended = (e) => {
-        atsrc(array[n]);
-        label.innerText = m - n;
-        n++;
-      }
-    }
-  }).catch(err => {
+  navigator.clipboard.readText().then(link => { algorithm(caller(link)); }).catch(err => {
     // maybe user didn't grant access to read from clipboard
     if (err == error) {
       error = true;
@@ -85,22 +94,21 @@ function script() {
   });
 }
 
+
 // input text player
 input[0].addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-    let re = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
-    let id = input[0].value.match(re)[7];
-    atsrc(id);
+    atsrc(caller(input[0].value));
   }
 });
 
 // rewind & forward
 button[0].addEventListener("click", function() {
   audio.currentTime += -10;
-})
+});
 button[1].addEventListener("click", function() {
   audio.currentTime += 10;
-})
+});
 // next track
 button[3].addEventListener("click", function() {
   atsrc(array[n]);
@@ -109,7 +117,12 @@ button[3].addEventListener("click", function() {
 });
 // store new id in queue
 button[2].addEventListener("click", function() {
-  script();
+  if (error == true) {
+    algorithm(caller(input[0].value));
+  }
+  else {
+    script();
+  }
 });
 
 // Queue Loop Auto Save
@@ -190,9 +203,4 @@ button[4].addEventListener("click", function()
 {
   localStorage.clear();
   location.reload();
-});
-
-// alert
-button[6].addEventListener("click", function() {
-  alert('YTIFY 2j\n\nCopy YouTube video link to clipboard to start playing automatically.\n\nBack & Forward : Rewind or Fast Forward 10 seconds.\n\nQueue: Click Queue & Add Next Playing links.\n\nNext: Skip to next track in queue.\n\nLoop: Set your track to be playing in a loop.\n\nAuto: Automatically intercept & play links.\n\nHQ 128kbps: Set audio quality to higher, costs 1MB per Minute.\n\nDelete: Clear all saved data.\n\nGithub: Fork this site and help improve.');
 });
