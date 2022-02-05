fa8ea13bbbadfe4d0a7303eb9be1c8614c5854ea
Animesh Nath <69345507+n-ce@users.noreply.github.com>
2022-02-05 09:23:31 +0530

    Queue count bug fix

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ function script() {
       array[m] = y = id;
       audio.onended = (e) => {
         atsrc(array[n]);
-        n++;
         label.innerText = m - n;
+        n++;
       }
     }
   }).catch(err => {
@@ -110,8 +110,8 @@ button[1].addEventListener("click", function() {
 // next track
 button[3].addEventListener("click", function() {
   atsrc(array[n]);
-  n++;
   label.innerText = m - n;
+  n++;
 });
 // store new id in queue
 button[2].addEventListener("click", function() {
