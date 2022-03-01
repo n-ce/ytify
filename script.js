a94607e3a29a62267771bd93eac0847c9fb6e4e8
n-ce <abrocks1120@gmail.com>
2022-03-01 16:50:31 +0530

    v3 : new - thumbnail setting, current track title. improved - thumbnail source, queue badge and more

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const button = document.getElementsByClassName('btn');
-const input = document.querySelectorAll('input');
-const label = document.querySelector('.label');
-const audio = document.querySelector('audio');
-const body = document.body.classList;
-const array = []; // id storage
+const input  = document.querySelectorAll('input');
+const badge  = document.querySelector('.badge');
+const audio  = document.querySelector('audio');
+const img    = document.querySelector('img');
+const body   = document.body.classList;
+const array  = []; // id storage
+const play   = localStorage.getItem('play');
+const regex  = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
+const url    = "https://noembed.com/embed?dataType=json&url=https://youtube.com/watch?v=";
 const interval = setInterval(script, 2000);
-const play = localStorage.getItem('play');
-const abstract = /(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
 
-let y; // store id
+let y; // store id for changes check
 let m; // queue count 
 let param; // algorithm parameter
 let n = 1; // current queue playing
-let c = 249; // quality value
+let c = 249; // quality codec value
+let l = true; // thumbnail boolean
 let queue; // queue boolean
 let error = "NotAllowedError: Read permission denied.";
 
@@ -20,61 +23,83 @@ function er() {
   error = true;
   clearInterval(interval);
   input[0].classList.remove('d-none');
+  button[2].style.display = 'none';
 }
 
-if (navigator.userAgent.indexOf('Firefox') != -1) { er(); }
+// Fallback for Firefox
+if (navigator.userAgent.indexOf('Firefox') !== -1) { er(); }
 
-// link to id converter
-function caller(val) { return val.match(abstract)[7]; }
+// link uid extractor
+function caller(val) { return val.match(regex)[7]; }
 
+// audio thumbnail title src
 function atsrc(x) {
-  //Playback
-  audio.src = "https://projectlounge.pw/ytdl/download?url=https://youtu.be/" + x + "&format=" + c;
+  // Playback --- Needs changed to youtube dl exec module
+  audio.src = `https://projectlounge.pw/ytdl/download?url=https://youtu.be/${x}&format=${c}`;
   audio.play();
-  //Thumbnail
-  document.querySelector('iframe').src = "https://youtube.com/embed/" + x;
+
+  fetch(url + x).then(res => res.json())
+    .then(data => {
+      // Thumbnail
+      if (l === true) { img.src = data.thumbnail_url; }
+      // Title
+      document.querySelector('h1').innerText = data.title;
+    });
+  // not to run again
   y = x;
 }
 
+// next track 
 function next() {
   if ((m - n) > -1) {
     atsrc(array[n]);
-    label.innerText = m - n;
+    badge.setAttribute("data-badge", m - n);
     n++;
   }
 }
 
+// local storage saving shortcut
 function save(name, key) { localStorage.setItem(name, key); }
 
+// forward backward
 function skip(t) { audio.currentTime += t; }
 
+// proper link intercepting algorithm
 function algorithm(param) {
   if (y != param) {
     // autoplay new id
     if (queue == undefined) { atsrc(param); }
     // queue new id
     else {
+      fetch(url + param).then(res => res.json())
+        .then(da => { queueList[m] = da.title; });
       m++;
-      label.innerText = m - n + 1;
+      badge.setAttribute("data-badge", m - n + 1);
       array[m] = y = param;
-      audio.onended = (e) => { next(); }
+      audio.onended = () => { next(); }
     }
   }
 }
 
-function script() { navigator.clipboard.readText().then(link => { algorithm(caller(link)) }).catch(err => { if (err == error) { er(); } }); }
+// clipboard copy
+function script() {
+  navigator.clipboard.readText()
+    .then(link => { algorithm(caller(link)) })
+    .catch(err => {
+      if (err == error) {
+        er();
+      }
+    });
+}
 
+// save current clipboard/input value to array storage
 function store() {
   if (error == true) { algorithm(caller(input[0].value)); }
   else { script(); }
 }
 
 // input text player
-input[0].addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    atsrc(caller(input[0].value));
-  }
-});
+input[0].oninput = () => { algorithm(caller(input[0].value)); }
 
 // Queue Loop Auto
 if (play == "loop") {
@@ -87,7 +112,6 @@ else if (play == "queue") {
   clearInterval(interval);
   button[2].classList.remove('d-none');
   button[3].classList.remove('d-none');
-  label.classList.remove('d-none');
   store();
 }
 else {
@@ -103,7 +127,6 @@ input[1].addEventListener("click", function() {
   clearInterval(interval);
   button[2].classList.remove('d-none');
   button[3].classList.remove('d-none');
-  label.classList.remove('d-none');
   save('play', "queue");
   store();
 });
@@ -141,22 +164,32 @@ input[4].onchange = function() {
 if (localStorage.getItem('theme') == "dark") {
   body.remove('bg-secondary');
   body.add('bg-dark');
+  for (w = 4; w < 7; w++)
+    button[w].classList.add('text-secondary');
   input[5].checked = true;
 }
-input[5].onchange = function() {
-  if (this.checked) {
-    body.remove('bg-secondary');
-    body.add('bg-dark');
-    save('theme', "dark");
-  } else {
-    body.add('bg-secondary');
-    body.remove('bg-dark');
-    save('theme', "secondary");
-  }
+input[5].onchange = () => {
+  input[5].checked == true ? save('theme', "dark") : save('theme', "off");
+  body.toggle('bg-secondary');
+  body.toggle('bg-dark');
+  for (w = 4; w < 7; w++)
+    button[w].classList.toggle('text-secondary');
+}
+
+// Thumbnail
+if (localStorage.getItem('thum') == "off") {
+  l = false;
+  img.classList.add('d-none');
+  input[6].removeAttribute('checked');
+}
+
+input[6].onchange = () => {
+  input[6].checked == true ? save('thum', "on") : save('thum', "off");
+  img.classList.toggle('d-none');
 }
 
 // Clear Settings
-button[4].addEventListener("click", function()
+button[4].addEventListener("click", () =>
 {
   localStorage.clear();
   location.reload();
