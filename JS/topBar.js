f8db278c0d19887eb304eb33058dd59270542680
Animesh Nath <69345507+n-ce@users.noreply.github.com>
2022-03-29 10:55:38 +0530

    Update topBar.js

--- JS/topBar.js
+++ JS/topBar.js
@@ -1,21 +1,28 @@
-const topBtn = document.getElementsByTagName('a');
+const topBtn = document.querySelectorAll('#configs a');
+
+// info
+
+topBtn[0].addEventListener('click', function() {
+  if (confirm("The About Page will open a page in a new tab. Continue?")) {
+    window.open("https://github.com/n-ce/ytify#about");
+  }
+});
 
 // Delete data
 
-topBtn[1].addEventListener('click',function(){
+topBtn[1].addEventListener('click', function() {
   localStorage.clear();
   location.reload();
 });
 
 // Toggle Input Bar
 
-topBtn[2].addEventListener('click',function(){
+topBtn[2].addEventListener('click', function() {
   document.querySelector('input').classList.toggle('hidden');
 });
 
 // Toggle Themes
 
-topBtn[3].addEventListener('click',function(){
+topBtn[3].addEventListener('click', function() {
   document.querySelector('#themes').classList.toggle('hidden');
 });
-
