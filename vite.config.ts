ba2e1995530b170b3cd21b2fa1b585cf110c09a2
n-ce <abrocks1120@gmail.com>
2023-08-28 19:10:00 +0530

    eruda integrated

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import type { PluginOption } from 'vite'
+
+// Set to false to disable eruda during development
+const eruda = true;
+
+const erudaInjector: PluginOption = {
+  name: 'erudaInjector',
+  transformIndexHtml: html => {
+    return {
+      html,
+      tags: [
+        {
+          tag: 'script',
+          attrs: {
+            src: '/node_modules/eruda/eruda'
+          },
+          injectTo: 'body'
+        }, {
+          tag: 'script',
+          injectTo: 'body',
+          children: 'eruda.init()'
+        }
+      ]
+    }
+  }
+}
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: (eruda && command === 'serve') ? [erudaInjector] : [],
+  }
+});
