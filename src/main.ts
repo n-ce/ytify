7ea21aee904e834b5a396c155400f6a594555e7a
n-ce <abrocks1120@gmail.com>
2023-08-29 22:37:25 +0530

    setup vite pwa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 import './style.css'
+document.body.addEventListener('click', () => {
+  self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
+  navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa'
-
 import type { PluginOption } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // Set to false to disable eruda during development
 const eruda = true;
@@ -28,8 +27,51 @@ const erudaInjector: PluginOption = {
   }
 }
 
+const manifest = {
+  "short_name": "ytify",
+  "name": "audio streaming with ytify",
+  "description": "32kb/s to 128kb/s youtube audio streaming website. Copy a youtube video link and listen to it as an audio totally free.",
+  "icons": [
+    {
+      "src": "logo_192px.png",
+      "type": "image/png",
+      "sizes": "192x192",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "logo_512px.png",
+      "type": "image/png",
+      "sizes": "512x512",
+      "purpose": "any maskable"
+    }
+  ],
+  "start_url": "/",
+  "display": "standalone",
+  "theme_color": "white",
+  "background_color": "#00bfff",
+  "share_target": {
+    "action": "/",
+    "method": "GET",
+    "params": {
+      "title": "title",
+      "text": "text",
+      "url": "url"
+    }
+  }
+}
+
 export default defineConfig(({ command }) => {
   return {
-    plugins: (eruda && command === 'serve') ? [erudaInjector, VitePWA({ registerType: 'autoUpdate' })] : [],
+    plugins: (eruda && command === 'serve') ? [
+      erudaInjector,
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+
+        manifest: manifest
+      })
+    ] : []
   }
 });
