60a6ef0d6c8d96c0183f959f77e83b476508d35e
n-ce <abrocks1120@gmail.com>
2023-08-29 22:53:53 +0530

    test 3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './style.css'
 document.body.addEventListener('click', () => {
+  if (!confirm('clear pwa cache ?'))
+    return;
   self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
   navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
+  location.reload();
 });
