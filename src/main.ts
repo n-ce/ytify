a6071dc7587d4f254b7cdab74fc3e9afe2d1d5d8
n-ce <abrocks1120@gmail.com>
2023-08-31 20:18:17 +0530

    fix build issue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import './style.css'
 import './nav.css'
 
-document.body.addEventListener('click', (e) => {
-  if (!e.target?.matches('body'))
-    return;
-  if (!confirm('clear pwa cache ?'))
-    return;
-  self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
-  navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
-  location.reload();
+document.getElementsByTagName('section')[4].addEventListener('click', () => {
+  if (confirm('clear pwa cache ?')) {
+    self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
+    navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
+    location.reload();
+  }
 });
