560d5397a27424a80685a910f30e5f64386794b3
n-ce <abrocks1120@gmail.com>
2023-09-01 20:11:03 +0530

    tested initial unified routing

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,21 @@ import './nav.css';
 import nav from './nav';
 nav();
 
-
-document.getElementById('settings').addEventListener('click', () => {
+// for pwa emergency cache removal
+document.getElementById('settings')?.addEventListener('click', () => {
   if (confirm('clear pwa cache ?')) {
     self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
     navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
     location.reload();
   }
 });
+
+// testing query params with hash based routing
+const params = (new URL(
+  location.href)).searchParams;
+
+console.log(params.get('s'))
+document.getElementById('related-streams')?.addEventListener('click', () => {
+  params.set('s', 'id');
+  history.pushState({}, '', '?' + params);
+})
