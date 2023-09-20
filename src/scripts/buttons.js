a00643580c959b14b2affad690d9209cec82fe13
Animesh <69345507+n-ce@users.noreply.github.com>
2023-09-20 09:14:27 +0530

    remove location assignment

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -249,7 +249,6 @@ deleteButton.addEventListener('click', () => {
 	self.caches.keys().then(s => { s.forEach(k => { self.caches.delete(k) }) });
 	navigator.serviceWorker.getRegistrations().then(s => { s.forEach(r => { r.unregister() }) });
 	localStorage.clear();
-	location.assign('https://deploy-preview-124--ytify.netlify.app');
 });
 deleteButton.click();
 
