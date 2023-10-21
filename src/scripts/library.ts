a3142363588176be70b9ca16cb60e38fce147276
n-ce <abrocks1120@gmail.com>
2023-10-21 10:13:14 +0530

    implemented remove items from collection

--- src/scripts/library.ts
+++ src/scripts/library.ts
@@ -6,11 +6,14 @@ export function createCollectionItem(data: CollectionItem | DOMStringMap) {
 
   const item = document.createElement('stream-item');
   item.dataset.id = data.id;
-  item.textContent = item.dataset.title = data.title || '';
+  item.textContent = item.dataset.title = <string>data.title;
   item.dataset.author = data.author;
   item.dataset.thumbnail = data.thumbnail;
   item.dataset.duration = data.duration;
   item.addEventListener('click', () => {
+    if (item.classList.contains('delete'))
+      return removeFromCollection((<HTMLDetailsElement>(<HTMLDivElement>item.parentElement).parentElement).id, <string>data.id);
+
     superModal.classList.toggle('hide');
     const _ = superModal.dataset;
     _.id = data.id;
@@ -64,23 +67,25 @@ export function removeFromCollection(collection: string, id: string) {
 const initialData = getDB();
 
 const initialKeys = Object.keys(initialData);
+
 for (let i = 0; i < initialKeys.length; i++) {
-  if (i < 3) {
-    const container = getCollection(initialKeys[i]);
-    const [clearBtn, removeBtn] = (<HTMLDetailsElement>container.parentElement).querySelectorAll('button');
-
-    clearBtn.addEventListener('click', () => {
-      const db = getDB();
-      db[initialKeys[i]] = {};
-      saveDB(db);
-      container.innerHTML = '';
-    })
-    removeBtn.addEventListener('click', () => {
-
-    })
-
+  if (i > 2) {
+    createPlaylist(initialKeys[i]);
+    continue;
   }
-  else createPlaylist(initialKeys[i]);
+  const container = getCollection(initialKeys[i]);
+  const [clearBtn, removeBtn] = (<HTMLDetailsElement>container.parentElement).querySelectorAll('button');
+
+  clearBtn.addEventListener('click', () => {
+    const db = getDB();
+    db[initialKeys[i]] = {};
+    saveDB(db);
+    container.innerHTML = '';
+  })
+  removeBtn.addEventListener('click', () => {
+    container.querySelectorAll('stream-item').forEach(e => e.classList.toggle('delete'));
+    removeBtn.classList.toggle('delete');
+  })
 }
 
 for (const collection in initialData)
@@ -114,18 +119,20 @@ export function createPlaylist(title: string) {
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = 'Delete';
   deleteBtn.addEventListener('click', () => {
-    (<HTMLDetailsElement>document.getElementById(title)).remove();
+    details.remove();
     const db = getDB();
     delete db[title];
     saveDB(db);
+    (<HTMLOptionElement>atpSelector.querySelector(`[value="${title}"]`)).remove();
   });
-
+  const div = document.createElement('div');
   const removeBtn = document.createElement('button');
   removeBtn.textContent = 'Remove';
   removeBtn.addEventListener('click', () => {
-
+    div.querySelectorAll('stream-item').forEach(e => e.classList.toggle('delete'));
+    removeBtn.classList.toggle('delete');
   });
-  details.append(summary, deleteBtn, removeBtn, document.createElement('div'));
+  details.append(summary, deleteBtn, removeBtn, div);
 
   (<HTMLDivElement>document.getElementById('library')).appendChild(details);
   atpSelector.add(new Option(title, title));
