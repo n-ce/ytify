3ee290d6302c2e11af8ddf173fb62344e0e271fd
n-ce <abrocks1120@gmail.com>
2024-05-04 12:44:13 +0530

    return piped data response

--- netlify/edge-functions/opengraph.ts
+++ netlify/edge-functions/opengraph.ts
@@ -5,26 +5,27 @@ export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
   const id = url.searchParams.get('s');
 
-  const response = await context.next();
-  const page = await response.text();
-  const injectionPoint = '<!-- OG Injection Point -->';
-
-
-  const standardOG = `
+  const og = id ? (await fetch('https://pipedapi.drgns.space/streams/' + id)
+    .then(res => res.json())
+    .then(data => `
+  <meta property="og:title" content="${data.title}">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://ytify.netlify.app?s=${id}">
+  <meta property="og:description" content="${data.description}">
+  <meta property="og:image" content="${data.thumbnailUrl}">
+  `)) : `
   <meta property="og:title" content="ytify">
   <meta property="og:type" content="website">
   <meta property="og:url" content="https://ytify.netlify.app">
   <meta property="og:description" content="48-160kbps Opus YouTube Audio Streaming Web App.">
   <meta property="og:image" content="/ytify_thumbnail_min.webp">
   `;
-  const newOG = `
-  <meta property="og:title" content="ytify">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://ytify.netlify.app?s=${id}">
-  <meta property="og:description" content="${'this is a test'}">
-  <meta property="og:image" content="https://i.ytimg.com/vi/${id}/hqdefault.jpg">
-  `
-  const og = id ? newOG : standardOG;
+
+  const response = await context.next();
+  const page = await response.text();
+  const injectionPoint = '<!-- OG Injection Point -->';
+
+
 
   const updatedPage = page.replace(injectionPoint, og);
   return new Response(updatedPage, response);
