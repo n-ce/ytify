83ec1b04b57d29793533191bc17ac9775aab35a7
n-ce <abrocks1120@gmail.com>
2024-05-04 12:24:40 +0530

    use injection point to dynamically add og tags

--- netlify/edge-functions/opengraph.ts
+++ netlify/edge-functions/opengraph.ts
@@ -4,23 +4,29 @@ export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
   const id = url.searchParams.get('s');
-  if (!id) return;
 
   const response = await context.next();
   const page = await response.text();
+  const injectionPoint = '<!-- OG Injection Point -->';
+
 
   const standardOG = `
+  <meta property="og:title" content="ytify">
+  <meta property="og:type" content="website">
   <meta property="og:url" content="https://ytify.netlify.app">
   <meta property="og:description" content="48-160kbps Opus YouTube Audio Streaming Web App.">
   <meta property="og:image" content="/ytify_thumbnail_min.webp">
   `;
   const newOG = `
+  <meta property="og:title" content="ytify">
+  <meta property="og:type" content="website">
   <meta property="og:url" content="https://ytify.netlify.app?s=${id}">
   <meta property="og:description" content="${'this is a test'}">
   <meta property="og:image" content="https://i.ytimg.com/vi/${id}/hqdefault.jpg">
   `
+  const og = id ? newOG : standardOG;
 
-  const updatedPage = page.replace(standardOG, newOG).replace('48-160kbps Opus YouTube Audio Streaming Web App.', 'test head change response');
+  const updatedPage = page.replace(injectionPoint, og);
   return new Response(updatedPage, response);
 };
 
