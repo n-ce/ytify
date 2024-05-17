3d113df9ad46bcacfa3382bb9ee4538427c1b86a
n-ce <abrocks1120@gmail.com>
2024-05-17 21:45:29 +0530

    show loading screen for upcoming queue query & fix update prompt layout

--- src/scripts/router.ts
+++ src/scripts/router.ts
@@ -1,4 +1,4 @@
-import { audio, queuelist, searchFilters, superInput, superModal } from "../lib/dom";
+import { audio, loadingScreen, queuelist, searchFilters, superInput, superModal } from "../lib/dom";
 import { fetchList, params } from "../lib/utils";
 import { appendToQueuelist } from "./queue";
 
@@ -6,12 +6,19 @@ const nav = document.querySelector('nav') as HTMLDivElement;
 const anchors = document.querySelectorAll('nav a') as NodeListOf<HTMLAnchorElement>;
 const routes = ['/', '/upcoming', '/search', '/library', '/settings', '/list'];
 const queueParam = params.get('a');
-const upcomingInjector = (param: string) => fetch(`${location.origin}/upcoming?id=${param}`)
-  .then(res => res.json())
-  .then(data => {
-    for (const stream of data)
-      appendToQueuelist(stream)
-  })
+
+
+function upcomingInjector(param: string) {
+  loadingScreen.showModal();
+
+  fetch(`${location.origin}/upcoming?id=${param}`)
+    .then(res => res.json())
+    .then(data => {
+      loadingScreen.close();
+      for (const stream of data)
+        appendToQueuelist(stream)
+    })
+}
 
 if (queueParam)
   addEventListener('DOMContentLoaded', () => upcomingInjector(queueParam));
