d718d1c7c95f6f6205f6c1f808200d40f7a802b2
n-ce <abrocks1120@gmail.com>
2024-05-17 19:20:07 +0530

    combine piped instances and invidious instances to reduce load for upcoming fetch requests

--- netlify/edge-functions/upcoming.ts
+++ netlify/edge-functions/upcoming.ts
@@ -16,9 +16,24 @@ function convertSStoHHMMSS(seconds: number) {
     hh + ':' : '') + `${mmStr}:${ssStr}`;
 }
 
-const pipedInstanceArray = async () => fetch('https://piped-instances.kavin.rocks').then(res => res.json())
-  .then(data => data.map((i: { api_url: string }) => i.api_url))
+const instanceArray = await fetch('https://piped-instances.kavin.rocks')
+  .then(res => res.json())
+  .then(data => data.map((i: { api_url: string }) => i.api_url + '/streams/'))
   .catch(() => ['https://pipedapi.kavin.rocks']);
+
+fetch('https://api.invidious.io/instances.json')
+  .then(res => res.json())
+  .then(data => {
+    const array = data
+      .filter((i: any) => i[1].cors && i[1].api)
+      .map((i: any) => i[1].url + '/api/v1/videos/');
+    instanceArray.push(array);
+  })
+  .catch(() => ['https://invidious.fdn.fr']);
+
+
+const getIndex = () => Math.floor(Math.random() * instanceArray.length);
+
 
 export default async (request: Request, _: Context) => {
 
@@ -26,23 +41,19 @@ export default async (request: Request, _: Context) => {
 
   if (!uid) return;
 
-  const instanceArray = await pipedInstanceArray();
-
-  const getIndex = () => Math.floor(Math.random() * instanceArray.length);
-
   const getData = async (
     id: string,
     api: string = instanceArray[getIndex()]
-  ): Promise<any> => await fetch(`${api}/streams/${id}`)
+  ): Promise<any> => await fetch(api + id)
     .then(res => res.json())
     .then(json => ({
       'id': id,
       'title': json.title,
-      'author': json.uploader,
-      'authorId': json.uploaderUrl.slice(9),
-      'duration': convertSStoHHMMSS(json.duration),
-      'thumbnailUrl': json.thumbnailUrl,
-      'source': api
+      'author': json.uploader || json.author,
+      'authorId': json.uploaderUrl.slice(9) || json.authorUrl,
+      'duration': convertSStoHHMMSS(json.duration || json.lengthSeconds),
+      'thumbnailUrl': json.thumbnailUrl || json.videoThumbnails[4].url,
+      'source': api + id
     }))
     .catch(() => getData(id))
 
