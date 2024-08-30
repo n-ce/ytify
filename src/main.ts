2ea0895e2e0de593e25ea01a5ad88e0cdde63e66
n-ce <abrocks1120@gmail.com>
2024-08-30 22:55:39 +0530

    fix boot freeze

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import './stylesheets/global.css';
-import './components/Settings';
-
+import './scripts/router';
 import './scripts/theme';
+import './scripts/search';
 import './scripts/queue';
+import './scripts/list';
 import './scripts/audioEvents';
 import './scripts/library';
+import './scripts/miniPlayer';
+import './components/Settings';
 
 addEventListener('DOMContentLoaded', async () => {
-  import('./scripts/router');
-
-  import('./scripts/search');
-
-  import('./scripts/list');
-
-  import('./scripts/miniPlayer');
-
   import('./scripts/start').then(mod => mod.default());
 });
