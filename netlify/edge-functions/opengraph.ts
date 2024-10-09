c23a91adeab308165b67b148541cb87489fc8efa
n-ce <abrocks1120@gmail.com>
2024-10-09 23:50:18 +0530

    merge list into opengraph_edge-functions

--- netlify/edge-functions/opengraph.ts
+++ netlify/edge-functions/opengraph.ts
@@ -1,27 +1,18 @@
 import { Context, Config } from '@netlify/edge-functions';
 
+
 export default async (request: Request, context: Context) => {
 
   const req = new URL(request.url);
 
-  if (!req.searchParams.has('s')) return;
+  const id = req.searchParams.get('s') || req.searchParams.get('playlists') || '';
 
-  const id = req.searchParams.get('s');
-
-  if (id?.length !== 11) return;
+  if (id.length < 11) return;
 
   const response = await context.next();
   const page = await response.text();
-  const instance = 'https://invidious.catspeed.cc';
-  const data = await fetch(instance + '/api/v1/videos/' + id).then(res => res.json());
-  const music = data.author.endsWith(' - Topic') ? 'https://wsrv.nl?w=180&h=180&fit=cover&url=' : '';
-  const thumbnail = music + data.videoThumbnails.find((v: { quality: string }) => v.quality === 'medium').url;
-  const newPage = page
-    .replace('48-160kbps Opus YouTube Audio Streaming Web App.', data.author.replace(' - Topic', ''))
-    .replace('"ytify"', `"${data.title}"`)
-    .replace(<string>context.site.url, `${context.site.url}?s=${id}`)
-    .replaceAll('/ytify_thumbnail_min.webp', thumbnail)
 
+  const newPage = await (id.length === 11 ? streamHandler(page, id) : playlistHandler(page, id));
 
   return new Response(newPage, response);
 };
@@ -29,3 +20,29 @@ export default async (request: Request, context: Context) => {
 export const config: Config = {
   path: '/*',
 };
+
+
+async function streamHandler(page: string, id: string) {
+  const instance = 'https://invidious.catspeed.cc';
+  const data = await fetch(instance + '/api/v1/videos/' + id).then(res => res.json());
+  const music = data.author.endsWith(' - Topic') ? 'https://wsrv.nl?w=180&h=180&fit=cover&url=' : '';
+  const thumbnail = music + data.videoThumbnails.find((v: { quality: string }) => v.quality === 'medium').url;
+  return page
+    .replace('48-160kbps Opus YouTube Audio Streaming Web App.', data.author.replace(' - Topic', ''))
+    .replace('"ytify"', `"${data.title}"`)
+    .replace('ytify.netlify.app', `ytify.netlify.app?s=${id}`)
+    .replaceAll('/ytify_thumbnail_min.webp', thumbnail);
+}
+
+
+async function playlistHandler(page: string, id: string) {
+
+  const data = await fetch('https://pipedapi.kavin.rocks/playlists/' + id).then(res => res.json());
+  return page
+    .replace('48-160kbps Opus YouTube Audio Streaming Web App.', data.uploader)
+    .replace('"ytify"', `"${data.name}"`)
+    .replace('ytify.netlify.app', `ytify.netlify.app/list?playlists=${id}`)
+    .replaceAll('/ytify_thumbnail_min.webp', data.relatedStreams[0].thumbnail);
+
+}
+
