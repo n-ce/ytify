b929a7803d3ce5d29d8bc6765b8884f099996061
n-ce <abrocks1120@gmail.com>
2024-10-09 21:48:14 +0530

    fix edge-functions

--- netlify/edge-functions/list.ts
+++ netlify/edge-functions/list.ts
@@ -9,14 +9,17 @@ export default async (request: Request, context: Context) => {
     return;
 
   const id = req.searchParams.get('playlists');
-
-
   const response = await context.next();
   const page = await response.text();
   const instance = 'https://inv.qilk.de';
   const data = await fetch(instance + '/api/v1/playlists/' + id).then(res => res.json());
+  const newPage = page
+    .replace('48-160kbps Opus YouTube Audio Streaming Web App.', data.author)
+    .replace('"ytify"', `"${data.title}"`)
+    .replace(<string>context.site.url, `${context.site.url}/list?playlists=${id}`)
+    .replaceAll('/ytify_thumbnail_min.webp', data.videos[0].videoThumbnails[0].url);
 
-  return new Response(data, response);
+  return new Response(newPage, response);
 }
 
 export const config: Config = {
