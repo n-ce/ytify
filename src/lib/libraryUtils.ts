742e8d7e3efeb66a5114f882f0e03812e6471b06
Animesh <69345507+n-ce@users.noreply.github.com>
2024-12-28 21:55:56 +0530

    Update libraryUtils.ts

--- src/lib/libraryUtils.ts
+++ src/lib/libraryUtils.ts
@@ -87,34 +87,49 @@ function renderDataIntoFragment(data: Collection, fragment: DocumentFragment) {
   }
 }
 
-export async function fetchCollection(collection: string | null) {
-
-  if (!collection) return;
+export async function fetchCollection(collection: string | null, shareId: string | null = '') {
 
   const fragment = document.createDocumentFragment();
 
-  const db = getDB();
-  const data = db[<'discover'>decodeURI(collection)];
+  if (collection) {
+    const db = getDB();
+    const data = db[<'discover'>decodeURI(collection)];
 
-  if (!data) {
-    alert('No items found');
-    return;
+    if (!data) {
+      alert('No items found');
+      return;
+    }
+
+    if (collection === 'discover')
+      for (const i in data)
+        if (data[i].frequency as number < 2)
+          delete db.discover?.[i];
+
+    saveDB(db);
+
+    renderDataIntoFragment(data, fragment);
+
+    if (!fragment.childElementCount) {
+      alert('No items found');
+      return;
+    }
+    store.list.id = collection;
+
+  } else {
+    // this means it does not exist in db and is a public collection
+
+    listBtnsContainer.className = 'publicPlaylist';
+    loadingScreen.showModal();
+    await fetch(`${location.origin}/public?id=${shareId}`)
+      .then(res => res.json())
+      .then(data => {
+        renderDataIntoFragment(data, fragment);
+      })
+      .catch(() => notify('Failed to load the shared list, it may consist of a corrupted stream'))
+      .finally(() => loadingScreen.close());
+
   }
 
-  if (collection === 'discover')
-    for (const i in data)
-      if (data[i].frequency as number < 2)
-        delete db.discover?.[i];
-
-  saveDB(db);
-
-  renderDataIntoFragment(data, fragment);
-
-  if (!fragment.childElementCount) {
-    alert('No items found');
-    return;
-  }
-  store.list.id = collection;
 
   listContainer.innerHTML = '';
   listContainer.appendChild(fragment);
@@ -135,7 +150,8 @@ export async function fetchCollection(collection: string | null) {
 
   listSection.scrollTo(0, 0);
   history.replaceState({}, '',
-    location.origin + location.pathname + '?collection=' + collection);
+    location.origin + location.pathname +
+    (collection ? ('?collection=' + collection) : ('?si=' + shareId)));
   document.title = (collection || 'Shared Playlist') + ' - ytify';
 }
 
@@ -269,4 +285,3 @@ export async function superCollectionLoader(name: SuperCollection) {
           loadSubList(name)
   );
 }
-
