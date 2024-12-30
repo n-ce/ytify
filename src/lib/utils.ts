2a0230ba05693cc26976dc93474705569d614151
Animesh <69345507+n-ce@users.noreply.github.com>
2024-12-30 15:50:15 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -88,8 +88,8 @@ export function convertSStoHHMMSS(seconds: number): string {
 }
 
 export async function getDownloadLink(id: string): Promise<string | null> {
-  const api = store.player.fallback || '';
-  if (!api) return;
+  const api = store.player.fallback;
+  if (!api) return '';
   const dl = await fetch(`${api}/download/${id}?f=${store.downloadFormat}`)
     .then(_ => _.json())
     .then(_ => {
