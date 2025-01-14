e5fb0ad4a27d320ee45801299496d4cf28e4bb64
n-ce <abrocks1120@gmail.com>
2025-01-14 13:05:47 +0530

    list preview fixes

--- netlify/edge-functions/listPreview.ts
+++ netlify/edge-functions/listPreview.ts
@@ -7,24 +7,19 @@ export default async (request: Request) => {
   return new Response(`     
 <!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="description"
-    content="View ${id} - ${type} in ytify">
+    content="View ${type} in ytify">
   <meta name="author" content="n-ce">
-  <meta name="thumbnail" content="/ytify_thumbnail_min.webp">
   <meta property="og:type" content="website">
-  <meta property="og:title" content="${type} | ${id} | ytify">
+  <meta property="og:title" content="${type} | ytify">
   <meta property="og:url" content="https://ytify.us.kg">
   <meta property="og:site_name" content="ytify">
   <meta property="og:description" content="View ${id} - ${type} in ytify">
-  <meta property="og:image" content="/ytify_thumbnail_min.webp">
-
-  <title>${type} | ${id} | ytify</title>
+  <title>${type} | ytify</title>
 </head>
-<script>location.replace('/?e=' + encodeURI('/list'+'${url.search}'))</script>
-</html>
+<script>location.replace('/?e=' + encodeURI(location.pathname + location.search))</script></html>
     `, {
     headers: { 'content-type': 'text/html' },
   });
