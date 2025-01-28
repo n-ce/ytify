32abd334ba1474b0179a185931543eed551fcf55
n-ce <abrocks1120@gmail.com>
2025-01-28 16:24:40 +0530

    testing netlify blobs for collection sharing

--- netlify/edge-functions/blobs.ts
+++ netlify/edge-functions/blobs.ts
@@ -3,29 +3,17 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 
 export default async (req: Request, context: Context) => {
-  const { uid } = context.params;
   const collection = getStore('clxn');
+  const data = await req.text();
+  const id = Date.now().toString();
+  const link = context.site.url + '/' + location.pathname + '?blob=' + id;
 
-
-  if (uid) {
-
-    const data = await collection.get(uid);
-    return new Response(JSON.stringify(data), {
-      headers: { 'content-type': 'application/json' },
-    });
-
-  }
-  else {
-
-    const data = await req.arrayBuffer();
-    const id = Date.now().toString();
-    const link = context.site.url + '/' + location.pathname + '?blob=' + id;
-
-    await collection.set(id, data);
-    return new Response(link);
-
-  }
-
+  await collection.set(id, data);
+  return new Response(link, {
+    headers: {
+      'content-type': 'text/plain'
+    }
+  });
 };
 
 
