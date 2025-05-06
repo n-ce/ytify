29551bb6d57b58a68b1c23871e180393193eec9a
Animesh <69345507+n-ce@users.noreply.github.com>
2025-05-06 16:56:41 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 
-declare const Version: string;
+declare const Build: string;
 declare const Locales: string[];
