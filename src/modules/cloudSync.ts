4b31a19afd872a4ba30d15c0759aa4a3077ca50d
n-ce <abrocks1120@gmail.com>
2025-05-28 19:44:26 +0530

    build 5.1

--- src/modules/cloudSync.ts
+++ src/modules/cloudSync.ts
@@ -64,6 +64,7 @@ export default function(dbhash: string, syncBtn: HTMLElement) {
         .finally(() => syncBtn.className = cls());
     }
   }
-  else fetchFromCloud();
+  else fetchFromCloud()
+    .finally(() => syncBtn.className = cls());
 
 }
