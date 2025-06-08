0397795e480c827b1d5666c15b261dc417aab64e
Animesh <69345507+n-ce@users.noreply.github.com>
2025-06-08 08:13:31 +0530

    Update playback.ts

--- src/components/Settings/playback.ts
+++ src/components/Settings/playback.ts
@@ -13,92 +13,102 @@ export default function() {
         <p>${i18n('settings_playback')}</p>
       </b>
 
-        ${Selector({
-    label: 'settings_audio_quality',
-    id: 'qualityPreference',
-    handler: async (e) => {
-      setState('quality', e.target.value as 'low' | 'medium' | 'high');
-      quickSwitch();
-    },
-    onmount: async (target) => {
-      target.value = state.quality || 'medium';
-    },
-    children: html`
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-          `
-  })}
+      ${Selector({
+        label: 'settings_audio_quality',
+        id: 'qualityPreference',
+        handler: async (e) => {
+          setState('quality', e.target.value as 'low' | 'medium' | 'high');
+          quickSwitch();
+        },
+        onmount: async (target) => {
+          target.value = state.quality || 'medium';
+        },
+        children: html`
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        `
+      })}
 
-        ${ToggleSwitch({
-    id: "prefetchSwitch",
-    name: 'settings_prefetch',
-    checked: Boolean(state.prefetch),
-    handler: () => {
-      setState('prefetch', !state.prefetch);
-    }
-  })}
+      ${ToggleSwitch({
+        id: "prefetchSwitch",
+        name: 'settings_prefetch',
+        checked: Boolean(state.prefetch),
+        handler: () => {
+          setState('prefetch', !state.prefetch);
+        }
+      })}
 
       ${!state.HLS ? html`
         ${Selector({
-    label: 'settings_codec_preference',
-    id: 'codecPreference',
-    handler: async (e) => {
-      setState('codec', e.target.value as 'opus' | 'any' | 'aac');
-      quickSwitch();
-    },
-    onmount: async (target) => {
-      target.value = state.codec;
-    },
-    children: html`
+          label: 'settings_codec_preference',
+          id: 'codecPreference',
+          handler: async (e) => {
+            setState('codec', e.target.value as 'opus' | 'any' | 'aac');
+            quickSwitch();
+          },
+          onmount: async (target) => {
+            target.value = state.codec;
+          },
+          children: html`
             <option value="opus">Opus</option>
             <option value="aac">AAC</option>
             <option value="any">Any</option>
           `
-  })}
+        })}
 
         ${ToggleSwitch({
-    id: "stableVolumeSwitch",
-    name: 'settings_stable_volume',
-    checked: Boolean(state.stableVolume),
-    handler: () => {
-      setState('stableVolume', !state.stableVolume);
-      quickSwitch();
-    }
-  })}
+          id: "stableVolumeSwitch",
+          name: 'settings_stable_volume',
+          checked: Boolean(state.stableVolume),
+          handler: () => {
+            setState('stableVolume', !state.stableVolume);
+            quickSwitch();
+          }
+        })}
+      
+        ${ToggleSwitch({
+          id: "enforcePipedSwitch",
+          name: 'settings_enforce_piped',
+          checked: state.enforcePiped,
+          handler: () => {
+            setState('enforcePiped', !state.enforcePiped);
+            quickSwitch();
+          }
+        })}
 
         ${ToggleSwitch({
-    id: "enforceProxySwitch",
-    name: 'settings_always_proxy_streams',
-    checked: state.enforceProxy,
-    handler: () => {
-      setState('enforceProxy', !state.enforceProxy);
-      quickSwitch();
-    }
-  })}
+          id: "enforceProxySwitch",
+          name: 'settings_always_proxy_streams',
+          checked: state.enforceProxy,
+          handler: () => {
+            setState('enforceProxy', !state.enforceProxy);
+            quickSwitch();
+          }
+        })}
       ` : html``}
 
       ${ToggleSwitch({
-    id: "HLS_Switch",
-    name: 'settings_hls',
-    checked: state.HLS,
-    handler: () => {
-      setState('HLS', !state.HLS);
-      notify(i18n('settings_reload'));
-    }
-  })}
+        id: "HLS_Switch",
+        name: 'settings_hls',
+        checked: state.HLS,
+        handler: () => {
+          setState('HLS', !state.HLS);
+          notify(i18n('settings_reload'));
+        }
+      })}
 
       ${ToggleSwitch({
-    id: "watchModeSwitch",
-    name: 'settings_watchmode',
-    checked: Boolean(state.watchMode),
-    handler: () => {
-      setState('watchMode',
-        state.watchMode ?
-          '' : '144p'
-      );
-    }
-  })}
+        id: "watchModeSwitch",
+        name: 'settings_watchmode',
+        checked: Boolean(state.watchMode),
+        handler: () => {
+          setState('watchMode',
+            state.watchMode ?
+              '' : '144p'
+          );
+        }
+      })}
     </div>
   `;
 }
