183e68b7badc2c628ab24288b901161e6d177a36
Animesh <69345507+n-ce@users.noreply.github.com>
2025-06-09 12:09:46 +0530

    Update linkPreview.ts

--- netlify/edge-functions/linkPreview.ts
+++ netlify/edge-functions/linkPreview.ts
@@ -9,7 +9,7 @@ export default async (request: Request, context: Context) => {
 
   const response = await context.next();
   const page = await response.text();
-  const keys = process.env.get('rkeys')!.split(',');
+  const keys = process.env.rkeys!.split(',');
 
   shuffle(keys);
 
