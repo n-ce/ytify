8b83d6171c77868bcf076c12eba4987462f5bcd8
error-reporting <huynhnhuthoang5@gmail.com>
2025-11-01 21:39:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { readdirSync } from 'fs';
 
 
 export default defineConfig(({ command }) => ({
-  Base: "/ytify-legacy/"
+  Base: '/ytify-legacy/' },
   define: {
     Locales: readdirSync(resolve(__dirname, './src/locales')).map(file => file.slice(0, 2)),
     Build: JSON.stringify(((d = new Date()) => `v7x8 ${d.getFullYear().toString().slice(-2)}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getDate().toString().padStart(2, '0')}`)())
