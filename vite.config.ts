ec9d3ee7b20b5ace3425497ab11c8fdc75c76a50
error-reporting <huynhnhuthoang5@gmail.com>
2025-11-01 21:03:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,13 @@ import { readdirSync } from 'fs';
 export default defineConfig(({ command }) => ({
   define: {
     Locales: readdirSync(resolve(__dirname, './src/locales')).map(file => file.slice(0, 2)),
-    Base: "/ytify-legacy/",
     Build: JSON.stringify(((d = new Date()) => `v7x8 ${d.getFullYear().toString().slice(-2)}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getDate().toString().padStart(2, '0')}`)())
   },
   plugins: [
     injectEruda(command === 'serve'),
     VitePWA({
       manifest: {
+        "base": "/ytify-legacy/",
         "short_name": "Ytify",
         "name": "Listen with ytify",
         "description": "32kb/s to 128kb/s youtube audio streaming website. Copy a youtube video link and listen to it as an audio totally free.",
