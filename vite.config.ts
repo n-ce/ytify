c8c07127f21c50c1118aff7052c803ce99d5e53d
n-ce <abrocks1120@gmail.com>
2025-11-02 08:14:21 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import { readdirSync } from 'fs';
 
 
 export default defineConfig(({ command }) => ({
-  base: '/ytify/',
+  base: process.env.VITE_BASE_PATH || '/',
   define: {
     Locales: readdirSync(resolve(__dirname, './src/locales')).map(file => file.slice(0, 2)),
     Version: JSON.stringify(
