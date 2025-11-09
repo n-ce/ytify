715d2b5611775f16bf170def0fde7cbf30742548
error-reporting <huynhnhuthoang5@gmail.com>
2025-11-09 21:47:19 +0700

    Update index.ts

--- src/components/Settings/index.ts
+++ src/components/Settings/index.ts
@@ -65,7 +65,7 @@ async function importSettings(e: Event) {
 if (params.has('reset')) {
   clearCache();
   localStorage.removeItem('store');
-  history.replaceState({}, '', location.origin + location.pathname.replace(/\/[^\/]*$/, '') || '/'));
+  history.replaceState({}, '', location.pathname);
 }
 
 
@@ -74,7 +74,7 @@ document.getElementById('exportSettingsBtn')!.addEventListener('click', exportSe
 document.getElementById('importSettingsBtn')!.addEventListener('change', importSettings);
 document.getElementById('restoreSettingsBtn')!.addEventListener('click', () => {
   localStorage.removeItem('store');
-  location.reload();
+  location.replace(location.origin + location.pathname.replace(/\/[^\/]*$/, '') || '/');
 });
 
 
