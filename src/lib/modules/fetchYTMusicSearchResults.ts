a4e1666107e9813d6332501a571afde9d05827f0
Animesh <69345507+n-ce@users.noreply.github.com>
2025-12-03 18:08:41 +0530

    Enabled backend for search

--- src/lib/modules/fetchYTMusicSearchResults.ts
+++ src/lib/modules/fetchYTMusicSearchResults.ts
@@ -115,8 +115,7 @@ export default async function(
   query: string,
 ): Promise<(YTStreamItem | YTListItem)[]> {
   const filter = config.searchFilter.substring(6); // remove "music_"
-  const api = 'https://ytify-backend.vercel.app';
-  const url = api + `/api/search?q=${encodeURIComponent(normalizeString(query))}&filter=${filter}`;
+  const url = Backend + `/api/search?q=${encodeURIComponent(normalizeString(query))}&filter=${filter}`;
 
   return fetchJson<YTMusicSearchResponse>(url)
     .then(data => {
