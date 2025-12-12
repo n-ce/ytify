5a4f0fe12cbb9e389d7d1b3123a74c89580db002
n-ce <abrocks1120@gmail.com>
2025-12-12 10:18:19 +0530

    Feedback test 3

--- src/lib/modules/feedback.ts
+++ src/lib/modules/feedback.ts
@@ -8,13 +8,12 @@ const handleSubmit = (event: Event) => {
   event.preventDefault();
 
   const form = event.target as HTMLFormElement;
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries()) as { [key: string]: string };
+  const message = (form.elements.namedItem('message') as HTMLTextAreaElement).value;
 
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({ "form-name": "feedback", ...data })
+    body: encode({ "form-name": "feedback", "message": message })
   })
     .then(() => {
       alert("Feedback submitted successfully!");
